337997f044e07633eeaeb4c53e9c4982e1ed4761
Ree <reezhuwy@gmail.com>
2022-12-23 10:43:07 +0800

    Update 1.modify.sh

--- preset-openwrt/1.modify.sh
+++ preset-openwrt/1.modify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #修改登录IP
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.33.1/g' package/base-files/files/bin/config_generate
 
 #复制uci-defaults脚本
 mkdir -p files/etc/uci-defaults
